f23ab39248a87193e0d1213ea21e91308d37163d
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-02-09 16:15:35 -0600

    Se genera el nuevo componente de un dialogo de nomina-ordinaria, donde muestra lo siguiente: Categoria Concepto Tipo SD/D Importe
    En este componente se detalle los conceptos y el total de sus pagos

--- src/app/nominaord-concepto-dialog/nominaord-concepto-dialog.ts
+++ src/app/nominaord-concepto-dialog/nominaord-concepto-dialog.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 
@@ -16,7 +17,7 @@ import { MatTableModule } from '@angular/material/table';
     MatTableModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
     ],
   templateUrl: './nominaord-concepto-dialog.html',
   styleUrl: './nominaord-concepto-dialog.css'
