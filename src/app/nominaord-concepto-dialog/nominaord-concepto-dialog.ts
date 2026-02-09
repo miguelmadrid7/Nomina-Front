b260d400a3b24087b94338a3efa4b8c80de38ff4
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-02-09 12:20:10 -0600

    Se genera el nuevo componente de un dialogo de nomina-ordinaria, donde muestra lo siguiente: Categoria Concepto Tipo SD/D Importe
    En este componente se detalle los conceptos y el total de sus pagos

---
+++ src/app/nominaord-concepto-dialog/nominaord-concepto-dialog.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-nominaord-concepto-dialog',
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+    ],
+  templateUrl: './nominaord-concepto-dialog.html',
+  styleUrl: './nominaord-concepto-dialog.css'
+})
+export class NominaordConceptoDialog {
+
+  cols = ['categoria','concepto', 'clave', 'importe',];
+  name = '';
+  totalImporte = 0;
+    
+  constructor(
+  @Inject(MAT_DIALOG_DATA) public data: {
+    empleadoId: number;
+    nombreEmpleado: string;
+    qnaProceso: number;
+    conceptos: any[];
+    curp: string;
+    rfc: string;
+  },
+  private ref: MatDialogRef<NominaordConceptoDialog>
+) {
+  this.totalImporte = (data.conceptos ?? [])
+    .reduce((acc, c) => acc + (Number(c.importeQnal) || 0), 0);
+}
+
+
+    close(){
+      this.ref.close();
+    }
+
+}
