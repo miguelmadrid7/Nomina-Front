363af8278048d2988a831258d9cb1ebb465bd772
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-02-24 11:48:44 -0600

    Se hace un refactor del codigo para que consuma las nuevas interfaces y sea mas facil la separacion de responsabilidades y que sea más seguro en escalabilidad

--- src/app/interfaces/pension-alimenticia-inter.ts
+++ src/app/interfaces/pension-alimenticia-inter.ts
@@ -4,24 +4,3 @@ export interface BeneficiarioAlimRequest {
   segundoApellido: string;
   nombre: string;
 }
-
-export interface BeneficiarioRequest {
-  tabEmpleadosId: number;
-  tabBeneficiariosAlimId: number;
-  formaAplicacion: 'P' | 'F';
-  factorImporte: number;
-  qnaini: number;
-  qnafin: number;
-  numeroDocumento: string;
-  numeroBenef?: number;
-}
-
-export interface IdResponse {
-  id: number;
-}
-
-
-export interface Banco {
-  id: number;
-  banco: string;
-}
