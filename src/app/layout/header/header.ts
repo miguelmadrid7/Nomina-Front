5433232e5a40957b78508b747d4a6cdbeec061b7
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-02-27 12:05:07 -0600

    Reajuste del header y se eliminan lo que estaban sobrando

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,15 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule, 
+    
+  ],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
 export class Header {
+
   @Output() toggleSidebarClick = new EventEmitter<void>();
-  onToggleClick() { this.toggleSidebarClick.emit(); }
+  
+  onToggleClick() { 
+    this.toggleSidebarClick.emit(); 
+  }
+
 }
