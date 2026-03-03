6f3e668e0a84026b9566b5ba70ce04d010d9a910
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-03-03 09:17:44 -0600

    Se eliminan las importaciones que no se utilizan

--- src/app/core/services/nomina-ordinaria.service.ts
+++ src/app/core/services/nomina-ordinaria.service.ts
@@ -1,8 +1,7 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { isPlatformBrowser } from '@angular/common';
 import { CalculationNomina } from '../../models/nomina-Ordinaria.model';
 import { ApiResponse } from '../../models/api-response.model';
 
