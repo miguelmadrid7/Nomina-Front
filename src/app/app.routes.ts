1803b16702d5bcf6afd9c53c115bb407a5777766
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-03-03 16:04:30 -0600

    Revert "Se implemnta el direccionamiento en ambas clases tanto del lado del sidebar y del app rutas"
    This reverts commit 7cb10b76db2b659e879187fac32b6f90c55f09ec.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -81,14 +81,6 @@ export const routes: Routes = [
                         data: { roles: [1] }
                     },
 
-                    {
-                        path: 'juicios/juicios-mercantiles',
-                        loadComponent: () =>
-                        import('./features/juicios-mercantiles/juicios-mercantiles')
-                            .then(m => m.JuiciosMercantiles),
-                        data: { roles: [1] }
-                    },
-
                 ]
         },
 
