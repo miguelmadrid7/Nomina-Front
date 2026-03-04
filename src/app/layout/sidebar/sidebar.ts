a9d60bb94a8169ba5987af014a77f102a8ec2a9b
Aaron Serna <aaronsernaz40@gmail.com>
2026-03-03 20:01:44 -0600

    Se hace refactor de código

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { LoginService } from '../../core/services/login.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'sidebar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrls: ['./sidebar.css']
 })
 export class Sidebar {
+
+  collapsed = false;
+
+  constructor(private loginService: LoginService) {}
+  
+   ngOnInit(): void {
+    const saved = localStorage.getItem('sidebar-collapsed');
+    this.collapsed = saved === 'true';
+  }
+
+  toggle(): void {
+    this.collapsed = !this.collapsed;
+    localStorage.setItem('sidebar-collapsed', String(this.collapsed));
+  }
+
+  hasAnyRole(roles: number[]): boolean {
+    return this.loginService.hasAnyRole(roles);
+  }
+
+  hasPermiso(nombre: string): boolean {
+    return this.loginService.hasPermiso(nombre);
+  }
+
+  hasModule(moduleId: number): boolean {
+    return this.loginService.hasModule(moduleId);
+  }
 
 }
