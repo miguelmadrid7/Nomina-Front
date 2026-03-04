07c4412cbb6369e637790de86dc5217f626e4963
Aaron Serna <aaronsernaz40@gmail.com>
2026-03-03 20:10:58 -0600

    Se hace refactor de código

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../../layout/sidebar/sidebar';
 import { ThemeSelector } from '../../layout/theme-selector/theme-selector';
+import { Header } from '../../layout/header/header';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     Sidebar,
     ThemeSelector,
+    Header,
   ],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
 export class Home {
 
+  @ViewChild(Sidebar) sidebar!: Sidebar;
+
+  toggleSidebar(): void {
+    this.sidebar.toggle();
+  }
 }
