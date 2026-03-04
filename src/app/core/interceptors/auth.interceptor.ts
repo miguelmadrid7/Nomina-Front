88ffe98f04caccd2f4895aa1722031d960213356
Aaron Serna <aaronsernaz40@gmail.com>
2026-03-03 19:52:21 -0600

    Se hace refactor de código

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,31 +1,23 @@
-import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+// src/app/interceptors/auth.interceptor.ts
+import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoginService } from '../services/login.service';
-import { environment } from '../../../environments/environment';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const loginService = inject(LoginService);
   const token = loginService.getToken();
+  const isAuthEndpoint = req.url.includes('/login'); // excluye login
 
-  const isAuthEndpoint = req.url.includes('/login');
-  const isApiCall = req.url.startsWith(environment.apiUrl);
-  const isPreflight = req.method === 'OPTIONS';
-
-  let reqToSend = req;
-  if (token && !isAuthEndpoint && isApiCall && !isPreflight) {
-    reqToSend = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-  }
-
-  return next(reqToSend).pipe(
-    catchError((err: HttpErrorResponse) => {
-      // No redirigir por errores en preflight
-      if (!isPreflight && (err.status === 401 || err.status === 403)) {
-        loginService.logout();
-        if (typeof window !== 'undefined') window.location.href = '/login';
+  if (token && !isAuthEndpoint) {
+    const authReq = req.clone(
+      { 
+        setHeaders: 
+        { 
+          Authorization: `Bearer ${token}` 
+        } 
       }
-      return throwError(() => err);
-    })
-  );
+    );
+    return next(authReq);
+  }
+  return next(req);
 };
