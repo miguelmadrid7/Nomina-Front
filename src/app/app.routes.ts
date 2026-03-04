43d827202a40a11880bf765abdf9fa3a9011db1d
Aaron Serna <aaronsernaz40@gmail.com>
2026-03-03 20:05:59 -0600

    Se hace refactor de código

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { ListEmpleado } from '../app/features/catalogos/empleado/list-empleado/l
 import { Login } from '../app/features/login/login';
 import { NominaOrdinaria } from '../app/features/nomina/nomina-ordinaria/nomina-ordinaria';
 import { NominaExtraordinaria } from '../app/features/nomina/nomina-extraordinaria/nomina-extraordinaria';
-import { PensionAlimenticia } from '../app/features/nomina/pension-alimenticia/pension-alimenticia';
-import { AuthGuard } from './core/auth/auth.guard';
+import { PensionAlimenticia } from './features/nomina/pension-alimenticia/pension-alimenticia';
+import { AuthGuard } from './core/guards/auth.guard';
 import { CalculoNominaComponent } from '../app/features/nomina/calculo-nomina/calculo-nomina.component';
 import { GenerarProductoComponent } from '../app/features/nomina/generar-producto/generar-producto.component';
 
@@ -20,23 +20,44 @@ export const routes: Routes = [
             canActivate: [AuthGuard],
             canActivateChild: [AuthGuard],
                 children: [
-    
-                  { path: 'empleados/list', component: ListEmpleado},
-                  { path: 'empleados/form', component: FormEmpleado},
 
                     {
-                        path: 'nomina/calculo',
-                        component: CalculoNominaComponent
+                        path: 'empleados/list',
+                        loadComponent: () =>
+                        import('./features/catalogos/empleado/list-empleado/list-empleado')
+                            .then(m => m.ListEmpleado)
+                    },
+
+                    {
+                        path: 'empleados/form',
+                        loadComponent: () =>
+                        import('./features/catalogos/empleado/form-empleado/form-empleado')
+                            .then(m => m.FormEmpleado)
+                    },
+
+                    {
+                        path: 'nomina/calculo-nomina-ordinaria',
+                        loadComponent: () =>
+                        import('./features/nomina/calculo-nomina/calculo-nomina.component')
+                            .then(m => m.CalculoNominaComponent),
+                            data: { roles: [1] }
                     },
 
                     {
                         path: 'nomina/ordinaria',
-                        component: NominaOrdinaria
+                        loadComponent: () =>
+                        import('./features/nomina/nomina-ordinaria/nomina-ordinaria')
+                            .then(m => m.NominaOrdinaria),
+                            data: { roles: [1] }
+
                     },
 
                     {
                         path: 'nomina/extraordinaria',
-                        component: NominaExtraordinaria
+                        loadComponent: () =>
+                        import('./features/nomina/nomina-extraordinaria/nomina-extraordinaria')
+                            .then(m => m.NominaExtraordinaria),
+                            data: { roles: [1] }
                     },
 
                     {
@@ -46,17 +67,23 @@ export const routes: Routes = [
 
                     {
                         path: 'nomina/generar-producto',
-                        component: GenerarProductoComponent
+                        loadComponent: () =>
+                        import('./features/nomina/generar-producto/generar-producto.component')
+                            .then(m => m.GenerarProductoComponent),
+                            data: { roles: [1] }
                     },
 
                     {
                         path: 'pension/alimenticia',
-                        component: PensionAlimenticia
+                        loadComponent: () =>
+                        import('./features/nomina/pension-alimenticia/pension-alimenticia')
+                            .then(m => m.PensionAlimenticia),
+                        data: { roles: [1] }
                     },
 
                 ]
         },
-        
+
       { path: '**', redirectTo: 'login' }
 ];
 
