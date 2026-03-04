d2f6bf0be520227f5cdf93691673d3e3a1024ded
Aaron Serna <aaronsernaz40@gmail.com>
2026-03-03 18:59:43 -0600

    Se hace refactor de código

---
+++ src/app/models/Emplado-inter.ts
@@ -0,0 +1,11 @@
+// Puedes ponerla en src/app/models/empleado-item.ts
+export interface EmpleadoItem {
+  id?: number;
+  rfc?: string; RFC?: string;
+  curp?: string; CURP?: string;
+  primer_apellido?: string; primerApellido?: string;
+  segundo_apellido?: string; segundoApellido?: string;
+  nombre?: string;
+  empleado?: string;
+  nombreCompleto?: string;
+}
