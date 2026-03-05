95dbe8d5c24fdec5e5e2469fa6a176deed3c6594
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-03-05 15:17:27 -0600

    Se ajusta y se agrega la url del componente para que se vea algo asi: Home / Nómina / Cálculo nómina ordinaria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,88 +8,80 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: Login},
 
-        {
-            path: 'home',
-            component: Home,
-            canActivate: [AuthGuard],
-            canActivateChild: [AuthGuard],
-            data: { breadcrumb: 'Home' },
-                children: [
+  {
+    path: 'home',
+    component: Home,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    data: { breadcrumb: 'Home' },
+    children: [
+      {
+        path: 'empleados/list',
+        loadComponent: () =>
+          import('./features/catalogos/empleado/list-empleado/list-empleado')
+            .then(m => m.ListEmpleado),
+        data: { breadcrumb: 'Empleados lista' }
+      },
+      {
+        path: 'empleados/form',
+        loadComponent: () =>
+          import('./features/catalogos/empleado/form-empleado/form-empleado')
+            .then(m => m.FormEmpleado),
+        data: { breadcrumb: 'Empleados formulario' }
+      },
 
-                    {
-                        path: 'empleados/list',
-                        loadComponent: () =>
-                        import('./features/catalogos/empleado/list-empleado/list-empleado')
-                            .then(m => m.ListEmpleado),
-                        data: { breadcrumb: 'Empleados lista' }
-                    },
+      {
+        path: 'nomina',
+        data: { breadcrumb: 'Nómina' },
+        children: [
+          {
+            path: 'calculo-nomina-ordinaria',
+            loadComponent: () =>
+              import('./features/nomina/calculo-nomina/calculo-nomina.component')
+                .then(m => m.CalculoNominaComponent),
+            data: { roles: [1], breadcrumb: 'Cálculo nómina ordinaria' }
+          },
+          {
+            path: 'ordinaria',
+            loadComponent: () =>
+              import('./features/nomina/nomina-ordinaria/nomina-ordinaria')
+                .then(m => m.NominaOrdinaria),
+            data: { roles: [1], breadcrumb: 'Visualizar cálculo ordinaria' }
+          },
+          {
+            path: 'extraordinaria',
+            loadComponent: () =>
+              import('./features/nomina/nomina-extraordinaria/nomina-extraordinaria')
+                .then(m => m.NominaExtraordinaria),
+            data: { roles: [1], breadcrumb: 'Cálculo nómina extraordinaria' }
+          },
+          {
+            path: 'generar-producto',
+            loadComponent: () =>
+              import('./features/nomina/generar-producto/generar-producto.component')
+                .then(m => m.GenerarProductoComponent),
+            data: { roles: [1], breadcrumb: 'Generar producto' }
+          }
+        ]
+      },
 
-                    {
-                        path: 'empleados/form',
-                        loadComponent: () =>
-                        import('./features/catalogos/empleado/form-empleado/form-empleado')
-                            .then(m => m.FormEmpleado),
-                        data: { breadcrumb: 'Empleados formulario' }
-                    },
+      {
+        path: 'pension/alimenticia',
+        loadComponent: () =>
+          import('./features/nomina/pension-alimenticia/pension-alimenticia')
+            .then(m => m.PensionAlimenticia),
+        data: { roles: [1], breadcrumb: 'Pensión alimenticia' }
+      },
 
-                    {
-                        path: 'nomina/calculo-nomina-ordinaria',
-                        loadComponent: () =>
-                        import('./features/nomina/calculo-nomina/calculo-nomina.component')
-                            .then(m => m.CalculoNominaComponent),
-                            data: { roles: [1], breadcrumb: 'Calculo nómina ordinaria'  }
-                    },
+      {
+        path: 'juicios/juicios-mercantiles',
+        loadComponent: () =>
+          import('./features/juicios/juicios-mercantiles/juicios-mercantiles')
+            .then(m => m.JuiciosMercantiles),
+        data: { roles: [1], breadcrumb: 'Juicios mercantiles' }
+      }
+    ]
+  },
 
-                    {
-                        path: 'nomina/ordinaria',
-                        loadComponent: () =>
-                        import('./features/nomina/nomina-ordinaria/nomina-ordinaria')
-                            .then(m => m.NominaOrdinaria),
-                            data: { roles: [1],  breadcrumb: 'Vizualizar calculo ordinaria'}
-
-                    },
-
-                    {
-                        path: 'nomina/extraordinaria',
-                        loadComponent: () =>
-                        import('./features/nomina/nomina-extraordinaria/nomina-extraordinaria')
-                            .then(m => m.NominaExtraordinaria),
-                            data: { roles: [1], breadcrumb: 'Calculo nómina extraordinaria'  }
-                    },
-
-                    {
-                        path: 'nomina/',
-                        component: NominaExtraordinaria
-                    },
-
-                    {
-                        path: 'nomina/generar-producto',
-                        loadComponent: () =>
-                        import('./features/nomina/generar-producto/generar-producto.component')
-                            .then(m => m.GenerarProductoComponent),
-                            data: { roles: [1], breadcrumb: 'Generar producto'  }
-                    },
-
-                    {
-                        path: 'pension/alimenticia',
-                        loadComponent: () =>
-                        import('./features/nomina/pension-alimenticia/pension-alimenticia')
-                            .then(m => m.PensionAlimenticia),
-                        data: { roles: [1], breadcrumb: 'Pension alimenticia'  }
-                    },
-
-                    {
-                        path: 'juicios/juicios-mercantiles',
-                        loadComponent: () =>
-                        import('./features/juicios/juicios-mercantiles/juicios-mercantiles')
-                            .then(m => m.JuiciosMercantiles),
-                        data: { roles: [1], breadcrumb: 'Juicios mercantiles'  }
-                    },
-
-                ]
-        },
-
-      { path: '**', redirectTo: 'login' }
+  { path: '**', redirectTo: 'login' }
 ];
-
-
