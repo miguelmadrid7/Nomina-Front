44b29bc1c1c44a1409dee05c6e70ac5695416701
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-03-18 14:28:39 -0600

    Se implemneta el nuevo endpoint para la actualizacion del modal

--- src/app/core/services/juicios-mercantiles.services.ts
+++ src/app/core/services/juicios-mercantiles.services.ts
@@ -17,22 +17,25 @@ export class JuiciosMercantilesService {
     return this.http.get<ApiResponse<BeneficiarioJMRequest[]>>(`${this.base}/beneficiarios/tab`);
   }
 
+   //Se obtiene la lista de los banco que hay en la bd y los muestra el combobox
+  getBancos() {
+    return this.http.get<ApiResponse<Banco[]>>(`${this.base}/catalogo/bancos`);
+  }
+
   //Se obtiene los beneficiarios del empleado seleccionado
   getobtenerBeneficiarios(empleadoId: number) {
     return this.http.get<ApiResponse<BeneficiarioNom[]>>(`${this.base}/beneficiarios/nom/${empleadoId}`);
   }
 
-  //Se hace post en el modal de
+  //Se hace post en el modal de dar de alta un beneficiario
   agregarBeneficiario(data: any) {
     return this.http.post<number>(`${this.base}/beneficiarios/nom`, data);
   }
 
-
-  //Se obtiene la lista de los banco que hay en la bd y los muestra el combobox
-  getBancos() {
-    return this.http.get<ApiResponse<Banco[]>>(`${this.base}/catalogo/bancos`);
+  //Se actualiza los datos de un beneficiario seleccionado
+  actualizarBeneficiario(id: number, data: any) {
+    return this.http.put<number>(`${this.base}/beneficiarios/nom/${id}`, data);
   }
-
 
 
 }
