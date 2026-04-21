78f0333a544c8263936c707cb0bffd6e0765469f
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-04-21 12:54:29 -0600

    Refactor de codigo

--- src/app/shared/directives/upperCase.directivas.ts
+++ src/app/shared/directives/upperCase.directivas.ts
@@ -1,16 +1,19 @@
 import { Directive, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appUppercase]'
 })
 export class UppercaseDirective {
 
+  constructor(private control: NgControl) {}
+
   @HostListener('input', ['$event'])
   onInput(event: Event) {
     const input = event.target as HTMLInputElement;
     const upper = (input.value ?? '').toUpperCase();
     if (input.value !== upper) {
-      input.value = upper;
+       this.control.control?.setValue(upper, { emitEvent: false });
     }
   }
 }
