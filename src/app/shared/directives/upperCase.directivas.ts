4dfef0157705452bf22adc01b6833a6acf5ca45d
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-04-28 09:49:28 -0600

    Se hace refactor de codigo

--- src/app/shared/directives/upperCase.directivas.ts
+++ src/app/shared/directives/upperCase.directivas.ts
@@ -2,18 +2,29 @@ import { Directive, HostListener } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
-  selector: '[appUppercase]'
+  selector: '[appUppercase]',
+  standalone: true,
 })
 export class UppercaseDirective {
 
-  constructor(private control: NgControl) {}
+  constructor(private ngControl: NgControl) {}
 
   @HostListener('input', ['$event'])
-  onInput(event: Event) {
+  onInput(event: Event): void {
     const input = event.target as HTMLInputElement;
+
+    const start = input.selectionStart ?? input.value.length;
+    const end = input.selectionEnd ?? input.value.length;
+
     const upper = (input.value ?? '').toUpperCase();
-    if (input.value !== upper) {
-       this.control.control?.setValue(upper, { emitEvent: false });
-    }
+    if (input.value === upper) return;
+
+    input.value = upper;
+
+    // actualiza el FormControl, sin re-disparar valueChanges
+    this.ngControl.control?.setValue(upper, { emitEvent: false });
+
+    // restaura cursor
+    input.setSelectionRange(start, end);
   }
 }
