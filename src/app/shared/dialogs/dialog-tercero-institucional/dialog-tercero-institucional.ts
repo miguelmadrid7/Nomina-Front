ea105de39d17ddb85c16681063429f0ba0e886ce
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-04-28 16:01:59 -0600

    Se agrega nuevo dialogo del registro de 3ros

---
+++ src/app/shared/dialogs/dialog-tercero-institucional/dialog-tercero-institucional.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-dialog-tercero-institucional',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatInputModule,
+    MatSelectModule,
+    MatNativeDateModule,
+
+  ],
+  templateUrl: './dialog-tercero-institucional.html',
+  styleUrl: './dialog-tercero-institucional.css'
+})
+export class DialogTerceroInstitucional {
+
+  form!: FormGroup;
+  tipoOrdenOptions = ['Alta', 'Baja'];
+
+  constructor(private fb: FormBuilder, private ref: MatDialogRef<DialogTerceroInstitucional>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      apellidoPaterno: [''],
+      apellidoMaterno: [''],
+      nombres: [''],
+      rfc: [''],
+      tipoOrden: [null],
+      qnaDesde: [''],
+      qnaHasta: [''],
+      fechaRegistro: [''],
+    });
+  }
+
+  guardar() {
+    
+  }
+
+  cerrar() {
+    this.ref.close();
+  }
+
+}
