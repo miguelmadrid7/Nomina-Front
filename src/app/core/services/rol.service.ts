5874d30736f4c7989bb6b7c063e502000cb941aa
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-08 12:24:32 -0600

    Refactor de codigo: Se agrega nuevo endpoint para crear los roles

--- src/app/core/services/rol.service.ts
+++ src/app/core/services/rol.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { ApiResponse } from "../../models/api-Response.model";
-import { Role } from "../../models/emplado.model";
+import { CreateRoleRequest, Role } from "../../models/emplado.model";
 import { Observable, map } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 
@@ -10,8 +10,14 @@ export class RolService {
   private base = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
-  getRoles(): Observable<Role[]> {
-    return this.http.get<ApiResponse<Role[]>>(`${this.base}/roles`).pipe(map(res => res.data ?? []));
-}
+    getRoles(): Observable<Role[]> {
+        return this.http.get<ApiResponse<Role[]>>(`${this.base}/roles`).pipe(map(res => res.data ?? []));
+    }
+
+    createRole(payload: CreateRoleRequest): Observable<ApiResponse<any>> {
+        return this.http.post<ApiResponse<any>>(`${this.base}/roles`, payload);
+    }
+
+
 
 }
