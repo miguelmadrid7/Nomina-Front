dd616e6904918619a321d00bc839cda795f0e392
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-08 15:22:58 -0600

    Refactor de codigo: Se integra el nuevo concepto con su respectivo rol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -115,7 +115,7 @@ export const routes: Routes = [
             loadComponent: () =>
               import('./features/terceros/terceros/terceros')
                 .then(m => m.Terceros),
-            data: { roles: [1, 3], breadcrumb: 'Registro terceros' }
+            data: { roles: [1, 3, 7], breadcrumb: 'Registro terceros' }
           },
           {    
             path: 'consulta-terceros',
