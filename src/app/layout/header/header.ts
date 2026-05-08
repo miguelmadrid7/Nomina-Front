7d0a32c6653f8a55db4d5d4fcad4c324f36f1041
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-08 15:49:07 -0600

    Refactor de codigo: Se cambia de posicion el boton de salir

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { LoginService } from '../../core/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -13,9 +15,19 @@ import { CommonModule } from '@angular/common';
 export class Header {
 
   @Output() toggleSidebarClick = new EventEmitter<void>();
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+  ) {}
   
   onToggleClick() { 
     this.toggleSidebarClick.emit(); 
+  }
+
+  logout(): void {
+    this.loginService.logout();
+    this.router.navigate(['/login'], { replaceUrl: true });
   }
 
 }
