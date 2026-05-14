88b4959e3da473436d89dc3b67cbab6c44481845
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-14 13:58:42 -0600

    Se integra el nuevo service para la gestion de los iconos

---
+++ src/app/core/services/icon.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ApiResponse } from '../../models/api-Response.model';
+import { IconRequest } from '../../models/gestion-core/icon.model';
+
+@Injectable({ providedIn: 'root' })
+export class IconService {
+  private base = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  createIcon(payload: IconRequest): Observable<ApiResponse<any>> {
+    return this.http.post<ApiResponse<any>>(`${this.base}/notifications/icons`,payload);
+  }
+}
