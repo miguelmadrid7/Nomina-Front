867a6620682b6598676fa9178fc3bbab592d063b
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-19 16:19:15 -0600

    Se integra el nuevo modulo para la consulta de las pensiones alimenticias

---
+++ src/app/features/pension-alimenticia/pension-alimenticia-consulta/pension-alimenticia-consulta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pension-alimenticia-consulta',
+  imports: [],
+  templateUrl: './pension-alimenticia-consulta.html',
+  styleUrl: './pension-alimenticia-consulta.css'
+})
+export class PensionAlimenticiaConsulta {
+
+}
