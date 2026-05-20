0b0d49907b853ddceff599ba81c3001040b6c222
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-20 11:40:49 -0600

    Se integran nuevas propiedades en mi modelo

--- src/app/models/beneficiario.model.ts
+++ src/app/models/beneficiario.model.ts
@@ -1,3 +1,5 @@
+import { Banco } from "./banco.model";
+
 export interface BeneficiarioRequest {
   tabEmpleadosId: number;
   tabBeneficiariosAlimId: number;
@@ -7,4 +9,45 @@ export interface BeneficiarioRequest {
   qnafin: number;
   numeroDocumento: string;
   numeroBenef?: number;
+  catBancoId: number;
+}
+
+export interface BeneficiarioDTO {
+  id: number;       
+  numeroBenef: number;  
+  empleado?: {
+    id: number;
+    nombre: string;
+    primerApellido: string;
+    segundoApellido: string;
+  };    
+  tabEmpleadosId: number;      
+  tabBeneficiariosAlimId: number;      
+  formaAplicacion: 'P' | 'C';   
+  factorImporte: number;      
+  qnaini: number;       
+  qnafin: number;      
+  numeroDocumento: string;    
+  banco?: Banco;   
+  beneficiarioAlim?: BeneficiarioAlimDTO;
+}
+
+export interface BeneficiarioAlimDTO {
+  id: number;
+  nombre: string;
+  primerApellido: string;
+  segundoApellido: string;
+  rfc: string;
+}
+
+export interface FilaBeneficiario {
+  id: number;
+  nombreEmpleado: string;
+  nombreBeneficiario: string;   
+  rfc: string;
+  noBeneficiario: number;
+  formaAplicacion: string;   
+  factorImporte: string;   
+  banco: string;   
+  qna: string;   
 }
