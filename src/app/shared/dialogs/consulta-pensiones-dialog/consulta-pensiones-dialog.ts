1dcda7ebcd48438b64e827310eb20a5d8bebaca9
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-05-22 12:40:48 -0600

    Refactor de codigo: Se integra un dialogo de confirmacion "Se actualizo correctamente"

--- src/app/shared/dialogs/consulta-pensiones-dialog/consulta-pensiones-dialog.ts
+++ src/app/shared/dialogs/consulta-pensiones-dialog/consulta-pensiones-dialog.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -12,9 +12,12 @@ import { MatCardModule } from '@angular/material/card';
 import { BeneficiarioRequest, FilaBeneficiario } from '../../../models/beneficiario.model';
 import { BeneficiarioAlimRequest } from '../../../models/pension-Alimenticia-model';
 import { switchMap } from 'rxjs/operators';
+import { UppercaseDirective } from '../../directives/upperCase.directivas';
+import { PensionAlimenDialog } from '../../../features/pension-alimenticia/pension-alimen-dialog/pension-alimen-dialog';
 
 @Component({
   selector: 'app-consulta-pensiones-dialog',
+  standalone: true,
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -25,6 +28,7 @@ import { switchMap } from 'rxjs/operators';
     MatInputModule,
     MatDialogModule,
     MatCardModule,
+    UppercaseDirective,
   ],
   templateUrl: './consulta-pensiones-dialog.html',
   styleUrl: './consulta-pensiones-dialog.css'
@@ -34,7 +38,7 @@ export class ConsultaPensionesDialog implements OnInit{
   form!: FormGroup;
   detalle: any = null;
 
-  constructor(private  ref: MatDialogRef<ConsultaPensionesDialog>, private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: FilaBeneficiario,  private pensionService: PensionAlimenticiaService){}
+  constructor(private  ref: MatDialogRef<ConsultaPensionesDialog>, private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: FilaBeneficiario,  private pensionService: PensionAlimenticiaService,  private dialog: MatDialog){}
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -68,16 +72,16 @@ export class ConsultaPensionesDialog implements OnInit{
           this.form.patchValue({
             nombreEmpleado: this.armarNombreEmpleado(d),
             nombreBeneficiario: this.armarNombreBeneficiario(d),
-            rfc: d.beneficiario_rfc || '—',
+            rfc: d.beneficiario_rfc,
             numeroBeneficiario: d.numero_benef ?? '',
             numeroOficio: d.numero_oficio ?? '',
             formaAplicacion: d.forma_aplicacion === 'P' ? 'Factor' : 'Importe fijo',
-            factorImporte: d.forma_aplicacion === 'P' ? `${((d.factor_importe ?? 0) * 100).toFixed(0)}` : `${Number(d.factor_importe ?? 0).toFixed(2)}`,
-            banco: d.nombre_banco || 'SIN BANCO',
-            clabe: d.numero_documento || '',
-            qnaInicio: d.qnaini ?? '',
-            qnaFin: d.qnafin ?? '',
-            numeroDocumento: d.numero_documento || ''
+            factorImporte: d.factor_importe,
+            banco: d.nombre_banco ?? '',
+            clabe: d.numero_documento ?? '',
+            qnaInicio: d.qnaini,
+            qnaFin: d.qnafin,
+            numeroDocumento: d.numero_documento ?? ''
           });
       },
 
@@ -137,13 +141,20 @@ export class ConsultaPensionesDialog implements OnInit{
     };
 
     this.pensionService.updateBeneficiarioAlim(alimId, alimPayload)
-      .pipe(
-        switchMap(() => this.pensionService.updateBeneficiario(nomId, nomPayload))
-      )
-      .subscribe({
-        next: () => {
-          console.log('✅ Actualizado correctamente');
-          this.ref.close(true);
+      .pipe(switchMap(() => this.pensionService.updateBeneficiario(nomId, nomPayload)))
+        .subscribe({
+          next: () => {
+            const dialogRef = this.dialog.open(PensionAlimenDialog, {
+              width: '350px',
+              data: {
+                message: 'Se actualizó correctamente',
+                type: 'success'
+              }
+            });
+
+            dialogRef.afterClosed().subscribe(() => {
+              this.ref.close(true); 
+            });
         },
         error: (err: any) => console.error('Error al actualizar', err)
       });
