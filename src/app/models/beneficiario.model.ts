3ee73b47835a3946fad0418ad28a2729955b21ca
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-06-04 14:44:49 -0600

    Refactor de codigo: Se hace separacion de logica se pasan los request a la carpeta de los request y se actualizan importaciones

--- src/app/models/beneficiario.model.ts
+++ src/app/models/beneficiario.model.ts
@@ -47,21 +47,3 @@ export interface FilaBeneficiario {
   qnaFin: number;
   mostrarEmpleado?: boolean; 
 }
-
-export interface BeneficiarioDetalleResponse {
-  id: number;
-  numero_benef: number;
-  tab_empleado_id: number;
-  tab_beneficiario_alim_id: number;
-  forma_aplicacion: 'P' | 'F';
-  factor_importe: number;
-  qnaini: number;
-  qnafin: number;
-  numero_documento: string;
-  cat_banco_id: number | null;
-  nombre: string;
-  primer_apellido: string;
-  segundo_apellido: string;
-  rfc: string;
-  nombre_banco: string | null;
-}
