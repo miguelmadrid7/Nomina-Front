fe0debb0a1c0badbf48b2d1cbb87f497313927df
Nomina-SEPH <miguel.maldonado@seph.gob.mx>
2026-06-04 14:50:17 -0600

    Refactor de codigo: Se hace separacion de logica se pasan los request a la carpeta de los request y se actualizan importaciones

---
+++ src/app/models/dto/beneficiarioDTO.model.ts
@@ -0,0 +1,24 @@
+import { Banco } from "../banco.model";
+import { BeneficiarioAlimDTO } from "../beneficiario.model";
+
+export interface BeneficiarioDTO {
+  id: number;       
+  numeroBenef: number;  
+  empleado?: {
+    id: number;
+    nombre: string;
+    primerApellido: string;
+    segundoApellido: string;
+    rfc: string;
+  };    
+  tabEmpleadosId: number;      
+  tabBeneficiariosAlimId: number;      
+  formaAplicacion: 'P' | 'F';   
+  factorImporte: number; 
+  numeroOficio?: string;     
+  qnaini: number;       
+  qnafin: number;      
+  numeroDocumento: string;    
+  banco?: Banco;   
+  beneficiarioAlim?: BeneficiarioAlimDTO;
+}
