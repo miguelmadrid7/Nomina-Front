221b9148b0da6f10d1bbd60314ed6f0d4efb4aa1
Nomina seph <nomina@seph.gob.mx>
2026-09-18 16:17:16 -0600

    feature: - Se integra dialogo de consulta de los lotes cargados

---
+++ src/app/consulta-terceros-lotes-dialog/consulta-terceros-lotes-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-terceros-lotes-dialog',
+  imports: [],
+  templateUrl: './consulta-terceros-lotes-dialog.html',
+  styleUrl: './consulta-terceros-lotes-dialog.css'
+})
+export class ConsultaTercerosLotesDialog {
+
+}
